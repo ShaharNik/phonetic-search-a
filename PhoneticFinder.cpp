020d7236fc7ed207f529da3952ba48ed89735f63
Shahar Niknazar <57103646+ShaharNik@users.noreply.github.com>
2020-03-20 12:17:19 +0200

    Add PhoneticFinder

---
+++ PhoneticFinder.cpp
@@ -0,0 +1,13 @@
+//
+// Created by shaharnik on 20/03/2020.
+//
+#include "PhoneticFinder.hpp"
+#include <iostream>
+using namespace std;
+namespace phonetic{
+    string find(string text, string word)
+    {
+        cout << "just testing" <<endl;
+        return "happy";
+    }
+}
---
+++ PhoneticFinder.hpp
@@ -0,0 +1,18 @@
+//
+// Created by shaharnik on 20/03/2020.
+//
+
+#ifndef PHONETIC_SEARCH_A_PHONETICFINDER_HPP
+#define PHONETIC_SEARCH_A_PHONETICFINDER_HPP
+
+#include <string>
+using namespace std;
+namespace phonetic{
+ string find(string text, string word);
+}
+
+
+
+
+
+#endif //PHONETIC_SEARCH_A_PHONETICFINDER_HPP
